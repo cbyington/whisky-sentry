ac5b7cfa7eecf04334a8d0a175268b46ed5c8f68
Chris Byington <chris.byington@gmail.com>
2018-09-05 21:14:22 -0700

    Update whisky-scraper.py

--- whisky-scraper.py
+++ whisky-scraper.py
@@ -1,3 +1,7 @@
+####################################
+## Libraries & Script Parameters ##
+####################################
+
 import datetime 
 import pytz
 from urllib.request import urlopen
@@ -7,12 +11,20 @@ from twilio.rest import Client
 # Twilio API credentials
 twilio_sid = 'AC7852bc7c5f9a918607ab6353a64624b9'
 twilio_token = 'ef0159ff649b8e3b46a142dac7b7945c'
-client = Client(twilio_sid,twilio_token)
+client = Client(twilio_sid,twilio_token) # stage the Twilio API call
 twilio_phone_number = '+18317038528'
-destination_phone_number = '+16508042890'
+#destination_phone_numbers = ['+16508042890','+18313595807'] # people to alert
+destination_phone_numbers = ['+16508042890']
 
 # URL of page to scrape
 page = 'https://www.klwines.com/productfeed?&productTypeCD=10&regionCD=&minprice=&maxprice=&page=1'
+
+# list of keywords to be matched to product name.  any of these will trigger a text to be sent
+search_words = ['Sazerac',
+				'Larue',
+				'Stagg',
+				'Handy']
+
 
 ####################
 ## Whisky Parsing ##
@@ -24,16 +36,14 @@ soup = BeautifulSoup(urlopen(page),'html.parser')
 # grab the data table from within the HTML object.  this is the info we're interested in inspecting
 rows = soup.find('tbody').find_all('tr')
 
-# instantiate empty dictionaries to hold spirit info; list of spirit names for insertion into SMS
-#new_spirits = {}
-spirits_to_alert = {}
+# empty list to hold spirit info.  This'll go into the SMS message
 list_of_spirits = []
 
-# calculate current time in UTC for comparison later to the time products were uploaded
+# calculate current time in UTC for comparison later to the time at which products were uploaded
 utc_tz = pytz.timezone("UTC")
 now_aware = utc_tz.localize(datetime.datetime.utcnow()) 
 
-# loop through each row in the table, cut out whitespace for each column
+# loop through each row in the table, cut out whitespace for each cell
 for row in rows:
 	cols = row.find_all('td')
 	cols = [x.text.strip() for x in cols]
@@ -41,33 +51,34 @@ for row in rows:
 	# cast scraped datetimes from naive to aware (UTC). datetimes are rendered (in browser) in PT but scraped in UTC :shrug:
 	cols[0] = utc_tz.localize(datetime.datetime.strptime(cols[0],'%m/%d/%Y %I:%M %p'))
 
-	# save date added, price, quantity, allocation columns in dict
-	#new_spirits[cols[3]] = [cols[0],cols[4],cols[5],cols[6]]
-	
-	# choose whether to write spirits into spirits to alert dictionary
-	if "Sold Out" not in cols[5] and ("limit" in cols[3] or "Sazerac" in cols[3] or "Larue" in cols[3] or "Stagg" in cols[3] or "Handy" in cols[3] or cols[6] == '1' or cols[6] == '2' or cols[6] == '3'):
-		if cols[0] >= now_aware + datetime.timedelta(minutes = -30):
-			spirits_to_alert[cols[3]] = [cols[0],cols[4],cols[5],cols[6]]
+	# choose whether to write spirits into list of spirits to alert on based on selection criteria
+	if cols[0] >= now_aware + datetime.timedelta(minutes = -30): # posted in last 30 mins
+		#if 'Sold Out' not in cols[5]:
+		if 'limit' in cols[3]: # "limit" is in the name - usually this is a bottle limit
+			list_of_spirits.append(cols[3] + " -- " + cols[4])
+		elif any(i in cols[3] for i in search_words): # if any of the search_words show up in the name
+			list_of_spirits.append(cols[3] + " -- " + cols[4])
+		elif any(x in cols[6] for x in ['1','2','3']): # if the allocation (bottle limit) is 1, 2, or 3
 			list_of_spirits.append(cols[3] + " -- " + cols[4])
 
-# compose SMS message body
-message = "New spirits to check out @ K&L! \n\nhttps://www.klwines.com/productfeed?&productTypeCD=10&regionCD=&minprice=&maxprice=&page=1 \n\n" 
-for spirit in range(len(list_of_spirits)):
-	message = message + str(list_of_spirits[spirit]) + "\n"
-
-# only send if there is something to send
-print(" ")
-if len(list_of_spirits) > 0:
-	print(message)
 
 ####################
 ## SMS Generation ##
 ####################
 
-	# indentation here means SMS will fire only if the list is non empty
-	client.messages.create(to=destination_phone_number,from_=twilio_phone_number,body=message)
-	print("Sent SMS")
+# compose SMS message body
+message = "New spirits to check out @ K&L! \n\nhttps://www.klwines.com/productfeed?&productTypeCD=10&regionCD=&minprice=&maxprice=&page=1 \n\n" 
+for spirit in list_of_spirits:
+	message = message + str(spirit) + "\n"
+
+# only send if there is something to send
+if len(list_of_spirits) > 0:
+	print(message) 
+
+	for phone_number in destination_phone_numbers:
+		#client.messages.create(to=destination_phone_numbers[i],from_=twilio_phone_number,body=message)
+		print("Sent SMS to " + str(phone_number))
 
 if len(list_of_spirits) == 0:
-	print(" ")
 	print("Nothing to see here!")
+	
