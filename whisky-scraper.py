4d1336259a66b0864ec5a975cd272f130150b87e
Chris Byington <chris.byington@gmail.com>
2018-09-11 17:04:54 -0700

    Update whisky-scraper.py

--- whisky-scraper.py
+++ whisky-scraper.py
@@ -54,7 +54,7 @@ for row in rows:
 	cols[0] = utc_tz.localize(datetime.datetime.strptime(cols[0],'%m/%d/%Y %I:%M %p'))
 
 	# choose whether to write spirits into list of spirits to alert on based on selection criteria
-	if cols[0] >= now_aware + datetime.timedelta(minutes = -720): # posted in last 30 mins
+	if cols[0] >= now_aware + datetime.timedelta(minutes = -30): # posted in last 30 mins
 		if 'Sold Out' not in cols[5]:
 			if 'limit' in cols[3]: # "limit" is in the name - usually this is a bottle limit
 				list_of_spirits.append(cols[3] + " -- " + cols[4])
