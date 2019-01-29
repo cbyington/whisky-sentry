048ba2a0cebb3e77f5aaa65438f0220b75db1e93
Chris Byington <chris.byington@gmail.com>
2019-01-28 19:02:20 -0800

    Push PG to production

--- whisky-scraper.py
+++ whisky-scraper.py
@@ -21,14 +21,14 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 # phone #s
 twilio_phone_number = '+18317038528'
-destination_phone_numbers = ['+16508042890'] # people to alert
+destination_phone_numbers = ['+16508042890','+18313595807'] # people to alert
 error_phone_number = '+16508042890' # phone # to which to send runtime error notifications
 
 # URL of page to scrape
 page = 'https://www.klwines.com/productfeed?&productTypeCD=10&regionCD=&minprice=&maxprice=&page=1'
 
 # list of keywords to be matched to product name.  any of these will trigger a text to be sent
-search_words = ['Sazerac','Larue','Stagg','Handy','K&L Exclusive','Gigondas']
+search_words = ['Sazerac','Larue','Stagg','Handy','K&L Exclusive']
 
 # any keyword in this list will result in the spirit not triggering an alert (even if it meets other criteria)
 blacklist_words = ['Rum','Vodka','Armagnac']
@@ -87,7 +87,7 @@ list_of_spirits = []
 utc_tz = pytz.timezone("UTC")
 now_aware = utc_tz.localize(datetime.datetime.utcnow()) 
 
-skus_recently_alerted_on = list(read_alerts(now_aware + datetime.timedelta(minutes = -120))) # don't alert on a product that was alerted on within the last 60m
+skus_recently_alerted_on = list(read_alerts(now_aware + datetime.timedelta(minutes = -60))) # don't alert on a product that was alerted on within the last 60m
 sku_blacklist = [str(item) for sublist in skus_recently_alerted_on for item in sublist]
 
 for row in rows: # loop through each row in the table, cut out whitespace for each cell
@@ -108,7 +108,7 @@ for row in rows: # loop through each row in the table, cut out whitespace for ea
 	# choose whether to write spirits into list of spirits to alert on based on selection criteria.  note to self on fields: 0 = time. 1 = sku. 3 = name. 4 = price. 5. q on hand. 6 = allocation.  
 	if 'Sold Out' not in cols[5]: # TODO: can these exclusion lists be put on one line?
 		if not any(b in cols[1] for b in sku_blacklist): # check if have alerted on the SKU in the last 60m.  if so, don't send another alert
-			if cols[0] >= now_aware + datetime.timedelta(minutes = -120): # require the product was posted in last 30 mins
+			if cols[0] >= now_aware + datetime.timedelta(minutes = -30): # require the product was posted in last 30 mins
 				if not any(j in cols[3] for j in blacklist_words): # exclude blacklisted spirits
 					if 'limit' in cols[3] or any(i in cols[3] for i in search_words) or any(z in cols[6] for z in ['1','2','3']): # alert if has limit in the name, has any of the search words, or as <= 3 allocation
 						list_of_spirits.append(cols[3] + " -- " + cols[4])
@@ -119,7 +119,7 @@ for row in rows: # loop through each row in the table, cut out whitespace for ea
 ####################
 
 # compose SMS message body
-message = "New spirits to check out @ K&L! (PG test!) \n\nhttps://www.klwines.com/productfeed?&productTypeCD=10&regionCD=&minprice=&maxprice=&page=1 \n\n" 
+message = "New spirits to check out @ K&L!  \n\nhttps://www.klwines.com/productfeed?&productTypeCD=10&regionCD=&minprice=&maxprice=&page=1 \n\n" 
 for spirit in list_of_spirits:
 	message = message + str(spirit) + "\n"
 
